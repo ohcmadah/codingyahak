8577a8ca595a304a959e05a56d4574ab9aefd3d4
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-12 08:02:26 +0900

    home 페이지 구현

--- nodejs/main.js
+++ nodejs/main.js
@@ -6,10 +6,11 @@ const app = http.createServer((request, response) => {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
   const pathname = url.parse(_url, true).pathname;
-  const title = queryData.id;
 
   if (pathname === "/") {
-    fs.readFile(`data/${title}`, "utf8", (err, description) => {
+    if (queryData.id === undefined) {
+      const title = "Welcome";
+      const description = "Hello, Node.js";
       const template = `
         <!doctype html>
         <html>
@@ -31,7 +32,32 @@ const app = http.createServer((request, response) => {
         `;
       response.writeHead(200);
       response.end(template);
-    });
+    } else {
+      const title = queryData.id;
+      fs.readFile(`data/${title}`, "utf8", (err, description) => {
+        const template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
