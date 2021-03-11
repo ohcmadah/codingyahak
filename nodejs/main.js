81766efda9b2d3de41bd75c9bef5097121ab4db9
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-12 07:54:53 +0900

    Not found

--- nodejs/main.js
+++ nodejs/main.js
@@ -5,39 +5,37 @@ const url = require("url");
 const app = http.createServer((request, response) => {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
   const title = queryData.id;
 
-  if (_url == "/") {
-    _url = "/index.html";
+  if (pathname === "/") {
+    fs.readFile(`data/${title}`, "utf8", (err, description) => {
+      const template = `
+        <!doctype html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+        </html>
+        `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
   }
-
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, "utf8", (err, description) => {
-    const template = `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-      </html>
-      `;
-    response.end(template);
-  });
 });
 
 const port = 3000;
