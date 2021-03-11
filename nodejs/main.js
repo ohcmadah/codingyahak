b121cdacaa89ffb83e27d47970bb9bea28f39093
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-11 10:30:55 +0900

    query data

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,21 +1,23 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const app = http.createServer((request, response) => {
-  let url = request.url;
+  let _url = request.url;
+  const queryData = url.parse(_url, true).query;
 
-  if (request.url == "/") {
-    url = "/index.html";
+  if (_url == "/") {
+    _url = "/index.html";
   }
 
-  if (request.url == "/favicon.ico") {
+  if (_url == "/favicon.ico") {
     return response.writeHead(404);
     response.end();
     return;
   }
 
   response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
+  response.end(queryData.id);
 });
 
 const port = 3000;
