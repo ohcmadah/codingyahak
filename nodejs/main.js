34a489425c8c43d084fcc4e8710e283374b8df00
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-13 00:42:19 +0900

    글 목록 출력하기

--- nodejs/main.js
+++ nodejs/main.js
@@ -11,31 +11,44 @@ const app = http.createServer((request, response) => {
     if (queryData.id === undefined) {
       const title = "Welcome";
       const description = "Hello, Node.js";
-      const template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-        </html>
-        `;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      const title = queryData.id;
-      fs.readFile(`data/${title}`, "utf8", (err, description) => {
+
+      fs.readdir("./data", (err, files) => {
+        let list = `<ul>`;
+        files.forEach((file) => {
+          list += `<li><a href="/?id=${file}">${file}</a></li>`;
+        });
+        list += `</ul>`;
+
         const template = `
+      <!doctype html>
+      <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+      </html>
+      `;
+
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", (err, files) => {
+        let list = "<ul>";
+        files.forEach((file) => {
+          list += `<li><a href="/?id=${file}">${file}</a></li>`;
+        });
+        list += "</ul>";
+
+        const title = queryData.id;
+        fs.readFile(`data/${title}`, "utf8", (err, description) => {
+          const template = `
           <!doctype html>
           <html>
             <head>
@@ -44,18 +57,15 @@ const app = http.createServer((request, response) => {
             </head>
             <body>
               <h1><a href="/">WEB</a></h1>
-              <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ol>
+              ${list}
               <h2>${title}</h2>
               <p>${description}</p>
             </body>
           </html>
           `;
-        response.writeHead(200);
-        response.end(template);
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
