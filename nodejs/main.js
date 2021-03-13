e8f6ae1d126ec8ed6378b325c40adf92e3a103d8
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-13 15:03:00 +0900

    출력 정보에 대한 보안

--- nodejs/main.js
+++ nodejs/main.js
@@ -4,6 +4,7 @@ const url = require("url");
 const qs = require("querystring");
 const path = require("path");
 const template = require("./lib/template");
+const sanitizeHtml = require("sanitize-html");
 
 const app = http.createServer((request, response) => {
   let _url = request.url;
@@ -30,20 +31,24 @@ const app = http.createServer((request, response) => {
       });
     } else {
       fs.readdir("./data", (err, files) => {
-        const title = queryData.id;
-        const list = template.list(files);
-        const filteredId = path.parse(title).base;
+        const filteredId = path.parse(queryData.id).base;
 
         fs.readFile(`./data/${filteredId}`, "utf8", (err, description) => {
+          const title = queryData.id;
+          const list = template.list(files);
+          const sanitizedTitle = sanitizeHtml(title);
+          const sanitizedDescription = sanitizeHtml(description, {
+            allowedTags: ["h1"],
+          });
           const html = template.html(
             title,
             list,
-            `<h2>${title}</h2>
-            <p>${description}</p>`,
+            `<h2>${sanitizedTitle}</h2>
+            <p>${sanitizedDescription}</p>`,
             `<a href="/create">create</a>
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
             <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
             </form>`
           );
