0c52f86d13fd78b2f41344adb1b3801d90711f2d
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-13 12:24:56 +0900

    파일생성과 리다이렉션

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
       </body>
     </html>
@@ -68,6 +70,46 @@ const app = http.createServer((request, response) => {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", (err, files) => {
+      const title = "WEB - create";
+      const list = templateList(files);
+
+      const template = templateHTML(
+        title,
+        list,
+        `
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `
+      );
+
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") {
+    let body = "";
+    request.on("data", (data) => {
+      body += data;
+    });
+    request.on("end", () => {
+      const post = qs.parse(body);
+      const title = post.title;
+      const description = post.description;
+      fs.writeFile(`./data/${title}`, description, "utf8", (err) => {
+        response.writeHead(302, {
+          location: `/?id=${title}`,
+        });
+        response.end("success");
+      });
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
