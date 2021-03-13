98201c842a0705667a6d17d009eedb93e0727eb9
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-13 13:44:18 +0900

    글 삭제 기능 완성

--- nodejs/main.js
+++ nodejs/main.js
@@ -65,7 +65,11 @@ const app = http.createServer((request, response) => {
             `<h2>${title}</h2>
             <p>${description}</p>`,
             `<a href="/create">create</a>
-            <a href="/update?id=${title}">update</a>`
+            <a href="/update?id=${title}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>`
           );
 
           response.writeHead(200);
@@ -160,6 +164,21 @@ const app = http.createServer((request, response) => {
           });
           response.end();
         });
+      });
+    });
+  } else if (pathname === "/delete_process") {
+    let body = "";
+    request.on("data", (data) => {
+      body += data;
+    });
+    request.on("end", () => {
+      const post = qs.parse(body);
+      const id = post.id;
+      fs.unlink(`./data/${id}`, (err) => {
+        response.writeHead(302, {
+          location: `/`,
+        });
+        response.end();
       });
     });
   } else {
