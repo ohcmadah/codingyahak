6beed5dd67384d9d93ab544597fb83cad0277809
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-16 10:04:56 +0900

    Redux devtools

---
+++ redux/main.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  fs.readFile("./with-redux.html", (error, data) => {
+    if (error) throw error;
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+app.listen(port);
