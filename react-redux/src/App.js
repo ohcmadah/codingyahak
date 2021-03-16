bec90ecea6fba15cff2b518ecda852f9307ea1a3
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-17 01:32:32 +0900

    redux 없는 react 컴포넌트 상태 연결하기

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -3,14 +3,25 @@ import "./App.css";
 import AddNumberRoot from "./components/AddNumberRoot";
 import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Root</h1>
-      <AddNumberRoot />
-      <DisplayNumberRoot />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    number: 0,
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot
+          onClick={(size) => {
+            this.setState({
+              number: this.state.number + size,
+            });
+          }}
+        />
+        <DisplayNumberRoot number={this.state.number} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-redux/src/components/AddNumberRoot.js
+++ react-redux/src/components/AddNumberRoot.js
@@ -6,7 +6,11 @@ export default class AddNumberRoot extends Component {
     return (
       <div>
         <h1>Add Number Root</h1>
-        <AddNumber />
+        <AddNumber
+          onClick={(size) => {
+            this.props.onClick(size);
+          }}
+        />
       </div>
     );
   }
