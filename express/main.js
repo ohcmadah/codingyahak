5b886de3b748ef6c2a1512f58a476d15bad9da0d
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-18 07:33:55 +0900

    상세 페이지 구현

--- express/main.js
+++ express/main.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const template = require("./lib/template.js");
+var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
 const port = 5000;
 
@@ -17,6 +19,32 @@ app.get("/", (request, response) => {
       `<a href="/create">create</a>`
     );
     response.send(html);
+  });
+});
+
+app.get("/page/:pageId", (req, res) => {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = req.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      res.send(html);
+    });
   });
 });
 
