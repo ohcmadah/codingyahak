64f731592cf500eaceccf7ccb8bc2289321b8af7
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-18 23:23:25 +0900

    라우터 - 파일로 분리

--- express/main.js
+++ express/main.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const template = require("./lib/template.js");
-const path = require("path");
-const sanitizeHtml = require("sanitize-html");
 const bodyParser = require("body-parser");
 const compression = require("compression");
-const router = express.Router();
+const topicRouter = require("./routes/topic");
 
 const app = express();
 const port = 5000;
@@ -20,6 +18,8 @@ app.get("*", (req, res, next) => {
   });
 });
 
+app.use("/topic", topicRouter);
+
 app.get("/", (req, res) => {
   const title = "Welcome";
   const description = "Hello, Node.js";
@@ -32,112 +32,6 @@ app.get("/", (req, res) => {
     `<a href="/topic/create">create</a>`
   );
   res.send(html);
-});
-
-app.get("/topic/create", (req, res) => {
-  const title = "WEB - create";
-  const list = template.list(req.list);
-  const html = template.HTML(
-    title,
-    list,
-    `
-      <form action="/topic/create" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `,
-    ""
-  );
-  res.send(html);
-});
-
-app.post("/topic/create", (req, res) => {
-  const post = req.body;
-  const title = post.title;
-  const description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    res.redirect(`/topic/${title}`);
-  });
-});
-
-app.get("/topic/update/:pageId", (req, res) => {
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    const title = req.params.pageId;
-    const list = template.list(req.list);
-    const html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/topic/update" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-});
-
-app.post("/topic/update", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/topic/${title}`);
-    });
-  });
-});
-
-app.post("/topic/delete", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    res.redirect("/");
-  });
-});
-
-app.get("/topic/:pageId", (req, res, next) => {
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      const title = req.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      const list = template.list(req.list);
-      const html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
-            <form action="/topic/delete" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-      );
-      res.send(html);
-    }
-  });
 });
 
 app.use((req, res, next) => {
