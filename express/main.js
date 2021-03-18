e4bd8117033da992c1c8555ac41438b5e74cbcb8
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-18 23:31:27 +0900

    라우터 - 파일로 분리 - index, 보안

--- express/main.js
+++ express/main.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const fs = require("fs");
-const template = require("./lib/template.js");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const helmet = require("helmet");
 const topicRouter = require("./routes/topic");
+const indexRouter = require("./routes/index");
 
 const app = express();
 const port = 5000;
@@ -11,6 +12,7 @@ const port = 5000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(helmet());
 app.get("*", (req, res, next) => {
   fs.readdir("./data", (err, fileList) => {
     req.list = fileList;
@@ -19,20 +21,7 @@ app.get("*", (req, res, next) => {
 });
 
 app.use("/topic", topicRouter);
-
-app.get("/", (req, res) => {
-  const title = "Welcome";
-  const description = "Hello, Node.js";
-  const list = template.list(req.list);
-  const html = template.HTML(
-    title,
-    list,
-    `<h2>${title}</h2>${description}
-     <img src="images/hello.jpg" style="width: 300px; display: block; margin-top: 10px" alt="hello" />`,
-    `<a href="/topic/create">create</a>`
-  );
-  res.send(html);
-});
+app.use("/", indexRouter);
 
 app.use((req, res, next) => {
   res.status(404).send("Sorry cant find that!");
