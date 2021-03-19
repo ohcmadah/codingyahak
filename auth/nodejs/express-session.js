022fe58f9669a252307a05d1b7428a3f3f26d022
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-20 00:45:08 +0900

    옵션

--- auth/nodejs/express-session.js
+++ auth/nodejs/express-session.js
@@ -12,26 +12,8 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {};
-  }
-
-  // get the url pathname
-  const pathname = parseurl(req).pathname;
-
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
-
-  next();
-});
-
 app.get("/foo", function (req, res, next) {
-  res.send("you viewed this page " + req.session.views["/foo"] + " times");
-});
-
-app.get("/bar", function (req, res, next) {
-  res.send("you viewed this page " + req.session.views["/bar"] + " times");
+  res.send(`Hello Session!`);
 });
 
 app.listen(3001);
