e2b30e2201fe0a6667c229ba58d755c6265be88e
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-20 01:06:52 +0900

    session store

--- auth/nodejs/express-session.js
+++ auth/nodejs/express-session.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const parseurl = require("parseurl");
 const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const app = express();
 
@@ -9,11 +10,17 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
+    store: new FileStore(),
   })
 );
 
 app.get("/foo", function (req, res, next) {
-  res.send(`Hello Session!`);
+  if (req.session.num === undefined) {
+    req.session.num = 1;
+  } else {
+    req.session.num += 1;
+  }
+  res.send(`Views: ${req.session.num}`);
 });
 
 app.listen(3001);
