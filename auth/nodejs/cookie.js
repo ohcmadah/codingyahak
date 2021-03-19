d3120ec59b577ccb36c73a563de1eb21e043e7b3
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-19 11:18:11 +0900

    쿠키의 읽기

--- auth/nodejs/cookie.js
+++ auth/nodejs/cookie.js
@@ -1,12 +1,16 @@
 const http = require("http");
+const cookie = require("cookie");
 
 const app = http.createServer((req, res) => {
-  //   res.writeHead(200, {
-  //     "Set-Cookie": [
-  //        "yummy_cookie=choco",
-  //        "tasty_cookie=strawberry"
-  //     ],
-  //   });
+  let cookies = {};
+  if (req.headers.cookie !== undefined) {
+    cookies = cookie.parse(req.headers.cookie);
+  }
+  console.log(cookies);
+  res.writeHead(200, {
+    "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"],
+  });
+
   res.end("cookie!");
 });
 
