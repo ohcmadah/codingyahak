c068400e3e0c57d13ff00d1c996ae3384c2fe4da
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-20 15:39:37 +0900

    인증상태를 UI에 반영

--- auth/express-auth/routes/index.js
+++ auth/express-auth/routes/index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const router = express.Router();
 const template = require("../lib/template.js");
 
+function authIsOwner(req, res) {
+  if (req.session.is_logined) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function authStatusUI(req, res) {
+  let authStatusUI = '<a href="/auth/login">login</a>';
+  if (authIsOwner(req, res)) {
+    authStatusUI = `${req.session.nickname} | <a href="/auth/logout">logout</a>`;
+  }
+  return authStatusUI;
+}
+
 router.get("/", (req, res) => {
-  console.log(req.session);
   const title = "Welcome";
   const description = "Hello, Node.js";
   const list = template.list(req.list);
@@ -12,7 +27,8 @@ router.get("/", (req, res) => {
     list,
     `<h2>${title}</h2>${description}
        <img src="images/hello.jpg" style="width: 300px; display: block; margin-top: 10px" alt="hello" />`,
-    `<a href="/topic/create">create</a>`
+    `<a href="/topic/create">create</a>`,
+    authStatusUI(req, res)
   );
   res.send(html);
 });
