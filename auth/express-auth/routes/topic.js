ee464f6b25b4abb75a238dd4779169892944a140
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-20 15:49:20 +0900

    접근제어

--- auth/express-auth/routes/topic.js
+++ auth/express-auth/routes/topic.js
@@ -7,6 +7,10 @@ const template = require("../lib/template.js");
 const auth = require("../lib/auth");
 
 router.get("/create", (req, res) => {
+  if (!auth.isOwner(req, res)) {
+    res.redirect("/");
+    return false;
+  }
   const title = "WEB - create";
   const list = template.list(req.list);
   const html = template.HTML(
@@ -30,6 +34,10 @@ router.get("/create", (req, res) => {
 });
 
 router.post("/create", (req, res) => {
+  if (!auth.isOwner(req, res)) {
+    res.redirect("/");
+    return false;
+  }
   const post = req.body;
   const title = post.title;
   const description = post.description;
@@ -39,6 +47,10 @@ router.post("/create", (req, res) => {
 });
 
 router.get("/update/:pageId", (req, res) => {
+  if (!auth.isOwner(req, res)) {
+    res.redirect("/");
+    return false;
+  }
   const filteredId = path.parse(req.params.pageId).base;
   fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
     const title = req.params.pageId;
@@ -66,6 +78,10 @@ router.get("/update/:pageId", (req, res) => {
 });
 
 router.post("/update", (req, res) => {
+  if (!auth.isOwner(req, res)) {
+    res.redirect("/");
+    return false;
+  }
   const post = req.body;
   const id = post.id;
   const title = post.title;
@@ -78,6 +94,10 @@ router.post("/update", (req, res) => {
 });
 
 router.post("/delete", (req, res) => {
+  if (!auth.isOwner(req, res)) {
+    res.redirect("/");
+    return false;
+  }
   const post = req.body;
   const id = post.id;
   const filteredId = path.parse(id).base;
